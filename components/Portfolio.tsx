d7df0d218d64376e06efb6b206ac602fa9058759
kaoruyuki <>
2024-05-13 16:19:23 +0900

    Add: Portfolio

---
+++ components/Portfolio.tsx
@@ -0,0 +1,25 @@
+import React from "react"
+import Image from "next/image"
+
+const Portfolio : React.FC = () => {
+  return (
+    <section id="portfolio" className="container mx-auto mt-24 flex w-full items-center justify-between px-8 md:px-14 lg:px-24">
+      <div className="w-full">
+        <h2 className="secondary-title">Portfolio</h2>
+        <p className="section-paragraph">
+          私が作ってきたWebアプリケーションがこちら
+        </p>
+        <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+          <Image src="/images/portfolio1.jpg" alt="portfolio1" width={1280} height={853} className="h-36 w-full cursor-pointer rounded-md object-cover lg:h-72"/>
+          <Image src="/images/portfolio2.jpg" alt="portfolio2" width={1280} height={853} className="h-36 w-full cursor-pointer rounded-md object-cover lg:h-72"/>
+          <Image src="/images/portfolio3.jpg" alt="portfolio3" width={1280} height={853} className="h-36 w-full cursor-pointer rounded-md object-cover lg:h-72"/>
+          <Image src="/images/portfolio4.jpg" alt="portfolio4" width={1280} height={853} className="h-36 w-full cursor-pointer rounded-md object-cover lg:h-72"/>
+          <Image src="/images/portfolio5.jpg" alt="portfolio5" width={1280} height={853} className="h-36 w-full cursor-pointer rounded-md object-cover lg:h-72"/>
+          <Image src="/images/portfolio6.jpg" alt="portfolio6" width={1280} height={853} className="h-36 w-full cursor-pointer rounded-md object-cover lg:h-72"/>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Portfolio
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors:{
+        secondary: "#9191A4"
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":

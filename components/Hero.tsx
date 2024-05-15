492dcc889a188257e0895933c58e171cf1b73f45
kaoruyuki <>
2024-05-15 11:44:51 +0900

    Fix: Hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,7 +6,7 @@ import Link from "next/link"
 
 const Hero : React.FC = () => {
   return (
-    <section id="home" className="container mx-auto flex w-full items-center justify-between px-8 md:px-14 lg:px-24">
+    <section id="home" className="container -z-10 mx-auto flex w-full items-center justify-between px-8 md:px-14 lg:px-24">
       <div className="flex flex-wrap md:flex-nowrap">
         <div className="mx-auto max-w-xl rounded-md border border-gray-200/30 bg-gray-800/30 p-4 shadow-lg backdrop-blur-lg md:my-36 lg:ml-20">
           <h1 className="text-center text-5xl font-bold md:text-left md:text-6xl lg:text-7xl">
@@ -15,7 +15,7 @@ const Hero : React.FC = () => {
             作ります
           </h1>
           <Link href="/#portfolio">
-            <button className="mx-auto mt-4 flex items-center rounded-lg bg-blue-600 px-6 py-4 font-bold text-white transition-all duration-300 space-x-3 hover:bg-blue-500 h-12">
+            <button className="mx-auto mt-4 flex h-12 items-center rounded-lg bg-blue-600 px-6 py-4 font-bold text-white transition-all duration-300 space-x-3 hover:bg-blue-500">
               <FontAwesomeIcon icon={faRocket} className="h-[20px] hover:text-blue-400"/>
               <span>もっと見る</span>
             </button>
